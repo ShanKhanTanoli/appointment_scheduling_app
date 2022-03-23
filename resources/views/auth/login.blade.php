1ad9c3891ae4350a3c0dbc65372a1d253776d7d1
Shan Khan <Shankhantanoli1@gmail.com>
2022-03-24 01:08:41 +0500

    Except front Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,18 +8,18 @@
                     <div class="row">
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto ">
                             <div class="card card-plain">
-                                <div class="card-header">
-                                    <h4 class="font-weight-bolder text-center">Login</h4>
+                                <div class="card-header text-center">
+                                    <h4 class="font-weight-bolder">Login</h4>
                                     <p class="mb-0">Enter your email and password to login</p>
                                 </div>
                                 <div class="card-body">
                                     <form method="POST" action="{{ route('login') }}" role="form">
                                         @csrf
                                         <div class="input-group input-group-outline mb-3">
-                                            <label class="form-label">Email</label>
                                             <input id="email" type="email"
                                                 class="form-control @error('email') is-invalid @enderror" name="email"
-                                                value="{{ old('email') }}" required autocomplete="email">
+                                                value="{{ old('email') }}" required autocomplete="email"
+                                                placeholder="Enter Email">
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -27,10 +27,9 @@
                                             @enderror
                                         </div>
                                         <div class="input-group input-group-outline mb-3">
-                                            <label class="form-label">Password</label>
                                             <input id="password" type="password"
                                                 class="form-control @error('password') is-invalid @enderror" name="password"
-                                                required autocomplete="new-password">
+                                                required autocomplete="password" placeholder="Enter Password">
                                             @error('password')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -40,19 +39,12 @@
                                         <div class="text-center">
                                             <button type="submit"
                                                 class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">
-                                                Register
+                                                Login
                                             </button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                                    <p class="mb-2 text-sm mx-auto">
-                                        Don't have an account ?
-                                        <a href="{{ route('register') }}"
-                                            class="text-primary text-gradient font-weight-bold">
-                                            Register
-                                        </a>
-                                    </p>
                                     <p class="mb-2 text-sm mx-auto">
                                         Forgot your password ?
                                         <a href="{{ route('password.request') }}"
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,53 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <main class="main-content  mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto ">
+                            <div class="card card-plain">
+                                <div class="card-header text-center">
+                                    <h4 class="font-weight-bolder">Reset Password</h4>
+                                    <p class="mb-0">Enter your email to reset password</p>
+                                </div>
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('password.email') }}" role="form">
+                                        @csrf
+                                        <div class="input-group input-group-outline mb-3">
+                                            <input id="email" type="email"
+                                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                                value="{{ old('email') }}" required autocomplete="email"
+                                                placeholder="Enter Email">
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit"
+                                                class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">
+                                                {{ __('Send Password Reset Link') }}
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                    <p class="mb-2 text-sm mx-auto">
+                                        Already have an Account ?
+                                        <a href="{{ route('login') }}"
+                                            class="text-primary text-gradient font-weight-bold">
+                                            Login here
+                                        </a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </section>
+    </main>
 @endsection
